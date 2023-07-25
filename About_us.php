79bb1283e9eddcfa0cd54adb52985aaeb02d672a
123subash <121342032+123subash@users.noreply.github.com>
2023-07-26 00:20:35 +0545

    Add files via upload

---
+++ About_us.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <?php include_once('header.php') ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dental</title>
+    <link rel="stylesheet" href="Dental.css">
+    <link rel="stylesheet" href="box.css">
+</head>
+
+<body>
+    <center>
+        <h3>
+            This Denaal clinic was stablished on 2016 with the aim of provide quality service to the people who need it.<br>
+            We are available 24/7 on your's service so contact us when you need any help which we provided.</h3>
+   
+    </center>
+   <hr>
+   <center><H1>our staff</H1></center>
+   <hr>
+    <section class="main">
+  
+  <div class="profile-card">
+    <div class="image">
+      <img src="jersy.jpg" alt="" class="profile-pic">
+    </div>
+    <div class="data">
+      <h2>Tilak Oli</h2>
+      <span>Dentist<br>
+      </span>
+    </div>
+    </section>
+    <section class="main">
+    <div class="profile-card">
+    <div class="image">
+      <img src="jersy.jpg" alt="" class="profile-pic">
+    </div>
+    <div class="data">
+      <h2>Sharmila Pathak </h2>
+      <span>Accountant<br>
+      </span>
+    </div>
+     </section>
+     <section class="main">
+    <div class="profile-card">
+    <div class="image">
+      <img src="jersy.jpg" alt="" class="profile-pic">
+    </div>
+    <div class="data">
+      <h2>Tilak oli </h2>
+      <span>Dentist<br>
+      </span>
+    </div>
+     </section>
+    
+</body>
+
+</html>
